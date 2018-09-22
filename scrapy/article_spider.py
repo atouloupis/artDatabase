47a707952aef01c53456882b83b3fbfd252220a0
atouloupis <atouloupis@yahoo.fr>
2018-09-22 23:03:42 +0200

    Scrape lot1.html + xpath
    Signed-off-by: atouloupis <atouloupis@yahoo.fr>

--- scrapy/article_spider.py
+++ scrapy/article_spider.py
@@ -1,7 +1,11 @@
 import scrapy
 class ArticleSpider(scrapy.Spider):
 	name = "article"
-	start_urls = ['http://blog.theodo.fr/2018/02/scrape-websites-5-minutes-scrapy']
+	start_urls = ['https://goodk.at/lot1.html']
 	def parse(self, response):
-		content = response.xpath(".//div[@class='entry-content']/descendant::text()").extract()
-		yield {'article': ''.join(content)}
+		content = response.xpath("//*[matches(@style,'left:6[0-9]{1}+.\d+px') and (@class='cls_002' or @class='cls_005')]").extract()
+		price = response.xpath("//*[matches(@style,'left:5[0-9]{2}.\d+px') and (@class='cls_002' or @class='cls_005')]").extract()
+		lot = response.xpath("//*[matches(@style,'left:[12345][0-9]{1}.\d+px') and (@class='cls_002' or @class='cls_005')]").extract()
+		yield {'description': ''.join(content)} 
+		yield {'prix': ''.join(price)}
+		yield {'lot': ''.join(lot)}
